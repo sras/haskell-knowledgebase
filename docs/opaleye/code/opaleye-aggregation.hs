17356bfffcaa51900e76f9a12f014f87c60cda5f
Sandeep.C.R <sandeepcr2@gmail.com>
2017-01-15 07:30:33 +0000

    Added opaleye aggregation

---
+++ docs/opaleye/code/opaleye-aggregation.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings      #-}
+{-# LANGUAGE TemplateHaskell        #-}
+
+module Main where
+
+import Opaleye
+import GHC.Int (Int64)
+import Data.Profunctor.Product
+import Data.Profunctor.Product.Default
+import Data.Profunctor.Product.TH           (makeAdaptorAndInstance)
+
+import Database.PostgreSQL.Simple
+import Database.PostgreSQL.Simple.FromField (FromField(..))
+
+import Prelude hiding (id)
+
+newtype UserId = UserId Int deriving (Show)
+
+data UserPoly id name email = User { id :: id, name :: name, email :: email } deriving (Show)
+
+type User = UserPoly UserId String String
+type UserPGW = UserPoly (Column PGInt4) (Column PGText) (Column PGText)
+type UserPGR = UserPoly (Column PGInt4) (Column PGText) (Column PGText)
+
+$(makeAdaptorAndInstance "pUser" ''UserPoly)
+
+userTable :: Table UserPGW UserPGR
+userTable = Table "users" (pUser User {
+    id = required "id",
+    name = required "name",
+    email = required "email"
+    }
+  )
+
+instance FromField UserId where
+  fromField field bs = UserId <$> fromField field bs
+
+instance QueryRunnerColumnDefault PGInt4 UserId where
+  queryRunnerColumnDefault = fieldQueryRunnerColumn
+
+getUserRows :: IO [UserPoly Int64 Int64 String]
+getUserRows = do
+  conn <- connect defaultConnectInfo { connectDatabase = "scratch"}
+  runQuery conn $ gQuery
+  where
+    gQuery :: Opaleye.Query (UserPoly (Column PGInt8) (Column PGInt8) (Column PGText))
+    gQuery = aggregate (pUser User { id = count, name = count, email = groupBy }) query
+    query :: Opaleye.Query UserPGR
+    query = queryTable userTable
+
+main :: IO ()
+main = do
+  rows <- getUserRows
+  putStrLn $ show rows
+
+-- Output
+-- [User {id = UserId 1, name = "John", email = "john@mail.com"},User {id = UserId
+--  2, name = "Bob", email = "bob@mail.com"},User {id = UserId 3, name = "Alice",
+-- email = "alice@mail.com"}]
